a922676f297c1441500a68ef357ef2c71f7c6aff
Natalia Jastrzębska <natalia@napcode.eu>
2018-06-26 12:11:39 +0200

    added developer dao and entity

---
+++ app/src/main/java/eu/napcode/resume/dao/DeveloperDao.kt
@@ -0,0 +1,17 @@
+package eu.napcode.resume.dao
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+
+@Dao
+abstract class DeveloperDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insert(vararg developer: DeveloperEntity)
+
+    @Query("SELECT * FROM developerentity WHERE id = :id")
+    abstract fun load(id: Int): LiveData<DeveloperEntity>
+}
---
+++ app/src/main/java/eu/napcode/resume/dao/DeveloperEntity.kt
@@ -0,0 +1,33 @@
+package eu.napcode.resume.dao
+
+import android.arch.persistence.room.Entity
+import com.google.gson.annotations.SerializedName
+
+@Entity
+data class DeveloperEntity(
+
+        val id: Int,
+
+        @field:SerializedName("name")
+        val name: String,
+
+        @field:SerializedName("surname")
+        val surname: String,
+
+        @field:SerializedName("role")
+        val role: String,
+
+        @field:SerializedName("mail")
+        val mail: String,
+
+        @field:SerializedName("home")
+        val home: String,
+
+        @field:SerializedName("github")
+        val github: String,
+
+        @field:SerializedName("gitlab")
+        val gitlab: String
+)
+
+
