d7f41a1389022681d29c5f0f292a2d31c222701c
Natalia Jastrzębska <natalia@napcode.eu>
2018-07-12 12:21:37 +0200

    added edu recycler view things

---
+++ app/src/main/java/eu/napcode/resume/ui/education/EducationAdapter.kt
@@ -0,0 +1,30 @@
+package eu.napcode.resume.ui.education
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.napcode.resume.R
+import eu.napcode.resume.model.Education
+import kotlinx.android.synthetic.main.item_education.view.*
+
+class EducationAdapter(private val educations: List<Education>) : RecyclerView.Adapter<EducationAdapter.ViewHolder>() {
+
+    private fun ViewGroup.inflate(layoutRes: Int): View {
+        return LayoutInflater.from(context).inflate(layoutRes, this, false)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, index: Int) = ViewHolder(viewGroup.inflate(R.layout.item_education))
+
+    override fun getItemCount() = educations.size
+
+    override fun onBindViewHolder(holder: ViewHolder, index: Int) = holder.bind(educations[index])
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(education: Education) = with(itemView) {
+            educationTextView.text = education.school
+        }
+    }
+
+}
