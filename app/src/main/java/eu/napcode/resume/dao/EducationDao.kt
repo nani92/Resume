5f99fba3312e42a957dad35b1301cffb11b56e6f
Natalia Jastrzębska <natalia@napcode.eu>
2018-07-12 12:48:58 +0200

    added sorting

--- app/src/main/java/eu/napcode/resume/dao/EducationDao.kt
+++ app/src/main/java/eu/napcode/resume/dao/EducationDao.kt
@@ -13,6 +13,6 @@ abstract class EducationDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insert(vararg education: EducationEntity)
 
-    @Query("SELECT * FROM educationentity")
+    @Query("SELECT * FROM educationentity ORDER BY endYear DESC")
     abstract fun load(): LiveData<List<EducationEntity>>
 }
