04f1f5b5914d44af33554f313b9d396f13b075ef
Natalia Jastrzębska <natalia@napcode.eu>
2018-07-24 14:16:58 +0200

    displaying project name

--- app/src/main/java/eu/napcode/resume/ui/project_details/ProjectDetailsActivity.kt
+++ app/src/main/java/eu/napcode/resume/ui/project_details/ProjectDetailsActivity.kt
@@ -5,18 +5,24 @@ import android.support.v7.app.AppCompatActivity
 import dagger.android.AndroidInjection
 import eu.napcode.resume.R
 import eu.napcode.resume.model.Project
+import kotlinx.android.synthetic.main.activity_project_details.*
 
 class ProjectActivity : AppCompatActivity() {
 
     companion object {
-        public const val ARG_PROJECT= "project"
+        const val ARG_PROJECT= "project"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_project)
+        setContentView(R.layout.activity_project_details)
         AndroidInjection.inject(this)
 
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
         var project = intent.getParcelableExtra<Project>(ARG_PROJECT)
+
+        projectTitleTextView.text = project.name
     }
 }
