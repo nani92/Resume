e866f38a822bea1ee259a605d793bcc02deec3a4
Natalia Jastrzębska <natalia@napcode.eu>
2018-07-25 10:27:23 +0200

    display project name and image

--- app/src/main/java/eu/napcode/resume/ui/project_details/ProjectDetailsActivity.kt
+++ app/src/main/java/eu/napcode/resume/ui/project_details/ProjectDetailsActivity.kt
@@ -1,6 +1,7 @@
 package eu.napcode.resume.ui.project_details
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
 import dagger.android.AndroidInjection
 import eu.napcode.resume.R
@@ -23,6 +24,10 @@ class ProjectActivity : AppCompatActivity() {
 
         var project = intent.getParcelableExtra<Project>(ARG_PROJECT)
 
+        appBarProjectTitleTextView.text = project.name
         projectTitleTextView.text = project.name
+
+        var begin = (topGuideline.layoutParams as ConstraintLayout.LayoutParams).guideBegin
+        topGuideline.setGuidelineBegin(begin + resources.getDimension(R.dimen.base_margin).toInt())
     }
 }
